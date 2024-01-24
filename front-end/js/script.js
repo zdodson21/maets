ba6e6eb36caab48aa12f477e8ef60e1865f8bc33
zdodson21 <zdodson21@gmail.com>
2024-01-23 19:14:44 -0500

    Added "Install Steam" button functionality to the about page

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -2,7 +2,7 @@ let installTrigger = false;
 
 function leaveClone() {
     if (installTrigger === false) {
-        alert('Warning! You are leaving the cloned website! Press the "Install Steam" button again to leave to store.steampowered.com');
+        alert('Warning! You are leaving the cloned website! This is not part of the cloned project, and is here to add functionality to features of the website. Press the "Install Steam" button again to leave to store.steampowered.com');
         installTrigger = true;
     } else {
         window.location.href = "https://store.steampowered.com/about/"
