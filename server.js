9e42858d6cb65296ebd98df10b6d92173e72f445
zdodson21 <zdodson21@gmail.com>
2024-04-07 14:36:13 -0400

    following tutorial, got server "running"

---
+++ server.js
@@ -0,0 +1,27 @@
+// test server using 'npx nodemon server.js'
+
+const express = require('express');
+const cors = require('cors');
+const server = express();
+
+server.use(cors()); // Helps with back end <-> front end interaction
+server.use(express.json()); // data can be shown in JSON format
+server.use(express.static('.')); // show front end information to browser
+
+// test
+server.get('/test', (req, res) => {
+    console.log('test');
+});
+
+// sign in
+server.post('/signin', (req, res) => {
+
+});
+
+
+// log in
+server.post('/login', (req, res) => {
+
+});
+
+server.listen(3000, () => console.log('server is running...'));
